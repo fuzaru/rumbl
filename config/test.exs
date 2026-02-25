4e86e4318a582042776b23268a4ce2906b81bc75
Ramsey Lagus <22-00862@g.batstate-u.edu.ph>
2026-02-25 10:29:35 +0800

    gitlab ci; test config change

--- config/test.exs
+++ config/test.exs
@@ -6,10 +6,11 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :rumbl, Rumbl.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "rumbl_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: System.get_env("POSTGRES_USER", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  database:
+    "#{System.get_env("POSTGRES_DB", "rumbl_test")}#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
